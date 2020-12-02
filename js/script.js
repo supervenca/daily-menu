53a43a75a0b8cf8fe3b10613297f4dcd51ba64c2
Ventseslava Shopova <supervenca@yandex.ru>
2020-12-02 15:18:03 +0200

    compiled with babel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
